7bc0a83d060226a9483db08c0cb44bd8e93e7543
thatalloguy <51132972+thatalloguy@users.noreply.github.com>
2022-04-03 14:53:57 +0200

    Update fun_game.py

--- fun_game.py
+++ fun_game.py
@@ -28,11 +28,27 @@ def cycle():
 
 
 def generate_flowers():
-    pass
+    print("HELLO WORLD")
 
+
+def bee_ai():
+    global start_x,start_y,end_y,honey_collected,end_x,travel,end2_y,end2_x
+    #print(str(end_x))
+    #print(str(end_y))
+    start_x = player.get_x()
+    start_y = player.get_y()
+
+    window.wait(bee_ai, 5)
+
+travel = True
+honey_collected = False
 window = Bee(69,69,title="A Bee life",resizable=False,fullscreen=True)
 window.config(color='light blue')
 generate_flowers()
+
+space = Space()
+space.gravity(10)
+
 tree = Entity(6, 1200, 900,0,shape="image",path="images/tree.png")
 player = Entity(1,500,500,100,shape="image",path="images/bee2.png")
 cloud1 = Entity(2,64,64,100,shape="image",path="images/cloud.png")
@@ -57,6 +73,15 @@ flower3.draw()
 cloud2.draw()
 cloud3.draw()
 cloud4.draw()
+start_x = player.get_x()
+start_y = player.get_y()
+#print(str(start_y))
+#print(str(start_x))
+end_x = flower1.get_x()
+end_y = flower1.get_y()
+end2_x = tree.get_x()
+end2_y = tree.get_y()
+window.wait(bee_ai, 1)
 window.wait(cycle, 1)
 window.update()
 
